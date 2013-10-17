ff188624df6550414e5e451263c871fa7e25193e
Nicholas Wargnier <nwargnier@gmail.com>
2013-10-17 00:07:02 -0700

    Initial Commit

---
+++ Normal_CalculatorTests/Normal_CalculatorTests.m
@@ -0,0 +1,34 @@
+//
+//  Normal_CalculatorTests.m
+//  Normal_CalculatorTests
+//
+//  Created by Nicholas Wargnier on 10/17/13.
+//  Copyright (c) 2013 Nicholas Wargnier. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Normal_CalculatorTests : XCTestCase
+
+@end
+
+@implementation Normal_CalculatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
